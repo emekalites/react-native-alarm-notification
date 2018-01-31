dbbb4cb0b1936a9fc4e46a761684d21c9941cda3
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2018-01-31 08:36:34 +0100

    removed override from createJSModules for rn >= 0.47 because of error during build

--- android/src/main/java/com/emekalites/react/alarm/notification/ANPackage.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/ANPackage.java
@@ -26,7 +26,6 @@ public class ANPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
