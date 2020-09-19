a963aec83b7d875df8eff34481690af6c15e3c18
Vince van Noort <vincevannoort@gmail.com>
2020-09-19 17:43:13 +0200

    move definition outside of try catch

--- android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
@@ -10,15 +10,13 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 public class AlarmDismissReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        try {
-            AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
+        AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
 
+        try {
             if (ANModule.getReactAppContext() != null) {
                 int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
                 ANModule.getReactAppContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("OnNotificationDismissed", "{\"id\": \"" + notificationId + "\"}");
-
                 alarmUtil.removeFiredNotification(notificationId);
-
                 alarmUtil.doCancelAlarm(notificationId);
             }
         } catch (Exception e) {
