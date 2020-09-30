915bc58f42c58e0475749181ed24702c881b1156
dakshabhalia <43106599+dakshabhalia@users.noreply.github.com>
2020-09-29 23:00:24 -0700

    AlarmUtil moved outside the catch block

--- android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
@@ -10,9 +10,8 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 public class AlarmDismissReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
         try {
-            AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
-
             if (ANModule.getReactAppContext() != null) {
                 int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
                 ANModule.getReactAppContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("OnNotificationDismissed", "{\"id\": \"" + notificationId + "\"}");
